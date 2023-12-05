370101facc42ae2a788396386c491a063aad07ec
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-12-05 11:11:21 +0900

    Bye World 추가 GoodHello World!! 추가

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Bye World");
         System.out.println("GoodHello World!!");
+        System.out.println("GoodHello World!!");
+        System.out.println("GoodHello World!!");
     }
 }
